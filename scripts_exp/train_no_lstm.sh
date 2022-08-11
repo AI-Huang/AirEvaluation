c39446dd166e4b9d165ae8dec24bca8693f1e7c9
Kan HUANG <kan.huang@connect.ust.hk>
2022-08-11 17:26:06 +0800

    add no_lstm codes

--- scripts_exp/train_no_lstm.sh
+++ scripts_exp/train_no_lstm.sh
@@ -9,8 +9,8 @@ hparam_stride="10 5" # 2
 for stride in $hparam_stride; do
 
     for sensor_index in $hparam_sensor_index; do
-        echo "sensor_index: $sensor_index; stride: $stride; --no-attention"
-        python train.py --model_name "WaveNet_LSTM" --no-attention --data_type=pm25 --sensor_index=$sensor_index --shuffle=True --stride=$stride --output_dir="./output"
+        echo "sensor_index: $sensor_index; stride: $stride; --no-lstm"
+        python train.py --model_name "WaveNet_LSTM" --no-lstm --data_type=pm25 --sensor_index=$sensor_index --shuffle=True --stride=$stride --output_dir="./output"
 
         # TODO send email from xx to yy when a run finished
         # python send_exper_email.py --stride --sensor_index --cur_time
--- scripts_exp/train_tests.sh
+++ scripts_exp/train_tests.sh
@@ -2,7 +2,10 @@
 # # 2022-2-12 18:05:16 Hong Kong
 
 # Normal training
-# python train.py --model_name "WaveNet_LSTM" --attention_type=official --data_type=pm25 --sensor_index=0 --shuffle=True --stride=10 --output_dir="./output"
+python train.py --model_name "WaveNet_LSTM" --attention_type=official --data_type=pm25 --sensor_index=0 --shuffle=True --stride=10 --output_dir="./output"
 
 # No attention
 python train.py --model_name "WaveNet_LSTM" --no-attention --data_type=pm25 --sensor_index=0 --shuffle=True --stride=10 --output_dir="./output"
+
+# No lstm
+python train.py --model_name "WaveNet_LSTM" --no-lstm --data_type=pm25 --sensor_index=0 --shuffle=True --stride=10 --output_dir="./output"
